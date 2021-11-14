adb02e3067fcc0bc15f1169bef5c23f59205504e
Turingcop <simon_evertsson@hotmail.com>
2021-11-14 04:09:19 +0100

    kmom06 final-ish release

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request as aliasRequest;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -49,7 +49,7 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
-    $request = aliasRequest::capture()
+    $request = Request::capture()
 ))->send();
 
 $kernel->terminate($request, $response);
